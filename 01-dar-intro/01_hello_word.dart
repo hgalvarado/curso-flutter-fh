8542acad7da91cbb191cd78bad7de94cb17eeb6c
hgalvarado <131572735+hgalvarado@users.noreply.github.com>
2023-04-26 20:49:59 -0600

    iniciando curso de flutter de fernando herrera

---
+++ 01-dar-intro/01_hello_word.dart
@@ -0,0 +1,9 @@
+void main() {
+//   var myName = "Henry";
+//   late final myName;
+//   const myName = "Henry";
+  String myName = "Henry";
+
+  print('Hola $myName');
+  print("Hola ${myName.toUpperCase()}");
+}
